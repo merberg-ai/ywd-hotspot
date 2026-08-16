0bbecfbdc85ddd3e5fc7a3f441096f038df42a34
merberg <j1mb3rg@gmail.com>
2026-08-16 15:53:53 -0700

    Add YWD shell environment helpers

---
+++ os/pi-gen/stage2/27-ywd-polish/files/ywd-env.sh
@@ -0,0 +1,5 @@
+# YWD-Hotspot OS shell environment
+export YWD_HOME=/opt/ywd-hotspot
+export YWD_CONFIG=/etc/ywd-hotspot/config.json
+export YWD_WEB=http://ywd-hotspot.local:8080/
+export YWD_GITHUB=https://github.com/merberg-ai/ywd-hotspot

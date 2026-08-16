fa682e2f3e8b57e397e5ae52bb77cc937e1ec51a
merberg <j1mb3rg@gmail.com>
2026-08-16 15:56:40 -0700

    Keep YWD prompt stable after user bashrc

--- os/pi-gen/stage2/27-ywd-polish/files/ywd-prompt.sh
+++ os/pi-gen/stage2/27-ywd-polish/files/ywd-prompt.sh
@@ -2,8 +2,18 @@
 case "$-" in *i*) ;; *) return ;; esac
 [ -n "${BASH_VERSION:-}" ] || return
 [ -z "${YWD_KEEP_PROMPT:-}" ] || return
-if [ "$(id -u)" -eq 0 ]; then
-  PS1='\[\e[1;35m\][YWD]\[\e[0m\] \u@\h \w # '
-else
-  PS1='\[\e[1;36m\][YWD]\[\e[0m\] \u@\h \w $ '
-fi
+
+__ywd_set_prompt() {
+  if [ "$(id -u)" -eq 0 ]; then
+    PS1='\[\e[1;35m\][YWD]\[\e[0m\] \u@\h \w # '
+  else
+    PS1='\[\e[1;36m\][YWD]\[\e[0m\] \u@\h \w $ '
+  fi
+}
+
+__ywd_set_prompt
+case ";${PROMPT_COMMAND:-};" in
+  *';__ywd_set_prompt;'*) ;;
+  ';;') PROMPT_COMMAND='__ywd_set_prompt' ;;
+  *) PROMPT_COMMAND="__ywd_set_prompt;${PROMPT_COMMAND}" ;;
+esac

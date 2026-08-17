8de2735b225e0edc04d95ffa1be0a68660e92c10
merberg <j1mb3rg@gmail.com>
2026-08-17 15:42:15 -0700

    Alpha18.2: load backup restore and plugin upload UI

--- web/app.js
+++ web/app.js
@@ -19,5 +19,6 @@
   loadStyle('/update.css?v=alpha12.1');
   loadStyle('/instrumentation.css?v=alpha12.1');
   loadStyle('/plugin-manager.css?v=alpha17');
-  load('/app-core.js', () => load('/talkgroups.js?v=alpha12.1', () => load('/ui-polish.js?v=alpha12.2', () => load('/update.js?v=alpha12.3', () => load('/update-progress.js?v=alpha16.1', () => load('/instrumentation.js?v=alpha12.1', () => load('/instrumentation-bootstrap.js?v=alpha12.1', () => load('/plugin-manager-render.js?v=alpha17', () => load('/plugin-package-actions.js?v=alpha16', () => load('/plugin-manager.js?v=alpha16', () => load('/plugin-config-actions.js?v=alpha16', () => load('/plugin-telemetry.js?v=alpha18'))))))))))));
+  loadStyle('/backup-restore.css?v=alpha18.2');
+  load('/app-core.js', () => load('/backup-restore.js?v=alpha18.2', () => load('/talkgroups.js?v=alpha12.1', () => load('/ui-polish.js?v=alpha12.2', () => load('/update.js?v=alpha12.3', () => load('/update-progress.js?v=alpha16.1', () => load('/instrumentation.js?v=alpha12.1', () => load('/instrumentation-bootstrap.js?v=alpha12.1', () => load('/plugin-manager-render.js?v=alpha18.2', () => load('/plugin-package-actions.js?v=alpha18.2', () => load('/plugin-package-upload.js?v=alpha18.2', () => load('/plugin-manager.js?v=alpha18.2', () => load('/plugin-config-actions.js?v=alpha16', () => load('/plugin-telemetry.js?v=alpha18'))))))))))))));
 })();

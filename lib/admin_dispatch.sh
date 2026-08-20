50943c88474b252b6c1c1835500b41ff2ce63cf6
merberg <j1mb3rg@gmail.com>
2026-08-20 05:54:12 -0700

    Route shutdown through narrow admin helper

--- lib/admin_dispatch.sh
+++ lib/admin_dispatch.sh
@@ -14,6 +14,9 @@ case "${1:-}" in
   ssh-keys-export|ssh-client-key-create)
     exec /usr/bin/python3 /opt/ywd-hotspot/app/lib/ssh_keys_admin.py "$@"
     ;;
+  shutdown)
+    exec /usr/bin/python3 /opt/ywd-hotspot/app/lib/system_admin.py "$@"
+    ;;
   plugin-system-set|plugin-set|plugin-config-save|plugin-runtime|plugin-package-install|plugin-package-uninstall|plugin-data-remove|plugin-package-upload|plugin-package-review|plugin-package-apply|plugin-package-remove)
     exec /usr/bin/python3 /opt/ywd-hotspot/app/lib/plugin_admin.py "$@"
     ;;
